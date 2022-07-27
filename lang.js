9795c172628bf703db171c5b0938fc5958290a9e
TheGhost <86230836+t4ebrian@users.noreply.github.com>
2022-07-27 21:30:48 +0300

    Add files via upload

---
+++ lang.js
@@ -0,0 +1,70 @@
+const LangArr = {
+    "main" : {
+        "ru": "Главная",
+        "en": "Main",
+    },
+    "buildings": {
+        "ru": "Постройки",
+        "en": "Buildings",
+    },
+    "NoRuBy": {
+        "ru": "Сервер недоступен на территории России и Беларуси",
+        "en": "Server does not working in Russia and Belarus",
+    },
+    //"close": {
+    //   "ru": "Закрыть",
+    //    "en": "Close",
+    //},
+    "sl": {
+        "ru": "StoneLight - ванильный сервер без приватов и доната",
+        "en": "Stonelight - vanilla server without private and donate",
+    },
+    "sur": {
+        "ru": "Выживайте, вступайте в кланы, находите новых друзей и просто весело проводите время :D",
+        "en": "Survive, join clans, meet nwe friends and just have fun :D",
+    },
+    "discord": {
+        "ru": "Вступить в Дискорд",
+        "en": "Join Discord",
+    },
+    "nobyru": {
+        "ru": "Сервер недоступен на территории России и Беларуси",
+        "en": "Server does not working in Russia and Belarus",
+    },
+    "develop": {
+        "ru": "Развивайтесь",
+        "en": "Develop",
+    },
+    "clans": {
+        "ru": "Присоединяйтесь к кланам",
+        "en": "Join clans",
+    },
+    "quests": {
+        "ru": "Выполняйте интересные ачивки",
+        "en": "Do intresting achievement",
+    },
+    "shops": {
+        "ru": "Открывайте собственные магазины",
+        "en": "Open self shops",
+    },
+    "project": {
+        "ru": "Создавайте маштабные проекты",
+        "en": "Make large projects",
+    },
+    "pirate": {
+        "ru": "На сервер можно зайти с пиратской версии Java или с Bedrock Minecraft-а",
+        "en": "You can enter the server from a pirated version of Java or from Bedrock Minecraft",
+    },
+    "sl1": {
+        "ru": "StoneLight - сервер ванильного выживания, имеющий некоторые дополнения и небольшие изменения игровых механик.",
+        "en": "Stonelight - server vanilla survive, which have some changes.",
+    },
+    "trading": {
+        "ru": "Измененены некоторые торги у жителей",
+        "en": "Changed some trade with villager",
+    },
+    "trade": {
+        "ru": "Алмазные вещи (броня, инструменты, оружие) жители будут продавать не только за изумруды, а и за алмазы (цена у жителей будет меньше чем при крафте на верстаке), книги зачарования будут продаваться за лазурит. Все инструменты и вся броня продаются без зачарований",
+        "en": "Villagers sell diamond things(armour, tools, weapon) not only for emeralds, but also for diamonds (the price of villagers will be less than when crafting on a crafting table) enchantment books will be sold for lapis lazuli. All tools and all armor are sold without enchantments",
+    },
+}
