9607ec02b2945d8951bad5fccd59489d5b961db9
TheGhost <86230836+t4ebrian@users.noreply.github.com>
2022-07-28 10:29:54 +0300

    Add files via upload

--- lang.js
+++ lang.js
@@ -21,7 +21,7 @@ const LangArr = {
     },
     "sur": {
         "ru": "Выживайте, вступайте в кланы, находите новых друзей и просто весело проводите время :D",
-        "en": "Survive, join clans, meet nwe friends and just have fun :D",
+        "en": "Survive, join clans, meet new friends and just have fun :D",
     },
     "discord": {
         "ru": "Вступить в Дискорд",
@@ -66,5 +66,113 @@ const LangArr = {
     "trade": {
         "ru": "Алмазные вещи (броня, инструменты, оружие) жители будут продавать не только за изумруды, а и за алмазы (цена у жителей будет меньше чем при крафте на верстаке), книги зачарования будут продаваться за лазурит. Все инструменты и вся броня продаются без зачарований",
         "en": "Villagers sell diamond things(armour, tools, weapon) not only for emeralds, but also for diamonds (the price of villagers will be less than when crafting on a crafting table) enchantment books will be sold for lapis lazuli. All tools and all armor are sold without enchantments",
+    },
+    "datapack": {
+        "ru": "Установлен датапак BlazeAndCave's, добавляющий в игру около 1000 новых достижений",
+        "en": "Blazeandcave's datapack has been installed, adding about 1000 new achievements to the game",
+    },
+    "totem": {
+        "ru": "Уменьшен шанс дропа тотема с призывателя со 100% до 5%",
+        "en": "Reduced the chance of totem drop from evolker from 100% to 5%",
+    },
+    "bags": {
+        "ru": "Добавлен крафт для мешков",
+        "en": "Added crafting for bags",
+    },
+    "craft-bags": {
+        "ru": "крафт мешка: ",
+        "en": "craft for bag: ",
+    },
+    "maps": {
+        "ru": "Странствующие торговцы иногда могут продать сундук с картой древнего города",
+        "en": "Wandering Trader can sometimes sell a chest with a map of an ancient city",
+    },
+    "rp": {
+        "ru": "Добавлен ресурспак сервера",
+        "en": "Added server resource pack",
+    },
+    "rpmore": {
+        "ru": "В ресурспак включены: различные шляпы, появляющиеся при переименовании тыкв, карты (переименованая бумага). Названия шляп можно посмотреть ",
+        "en": "The resource pack includes: various hats that appear when renaming pumpkins, maps (renamed paper). The names of the hats can be viewed ",
+    },
+    "here": {
+        "ru": "здесь",
+        "en": "here",
+    },
+    "head": {
+        "ru": "Игроки при убийстве дропают головы",
+        "en": "Players drop their heads when killing",
+    },
+    "arm": {
+        "ru": "Редактор стоек для брони",
+        "en": "Armor Stand Editor",
+    },
+    "rmb": {
+        "ru": "Нажатие ПКМ с кремнем с руки открывает интуитивно понятное меню управления",
+        "en": "Pressing the RMB with a flint from the hand opens an intuitive control menu",
+    },
+    "banner": {
+        "ru": "Баннеры можно располагать горизонтально как флаги",
+        "en": "Banners can be positioned horizontally as flags",
+    },
+    "block": {
+        "ru": "Поддерживаемые блоки: заборы, стены, стеклянные панели, железные решетки, громоотводы. Присядьте и щелкните баннер на одном из поддерживаемых блоков, чтобы разместить его в качестве флага",
+        "en": "Supported blocks: fences, walls, glass panels, iron grilles, lightning rods. Sit down and click a banner on one of the supported blocks to place it as a flag",
+    },
+    "invise": {
+        "ru": "У игрока с невидимостью нет частиц эффекта",
+        "en": "A player with invisibility has no effect particles",
+    },
+    "rules": {
+        "ru": "Основные правила",
+        "en": "Basic rules",
+    },
+    "break": {
+        "ru": "Запрещён гриф",
+        "en": "Illegal griff",
+    },
+    "cheat": {
+        "ru": "Запрещённы читы",
+        "en": "Illegal cheats",
+    },
+    "acc": {
+        "ru": "Нельзя иметь более 1 аккаунта",
+        "en": "You cannot have more than 1 account",
+    },
+    "offense": {
+        "ru": "Нельзя оскорблять и токсичить в чатах",
+        "en": "You can not insult and be toxic in chats",
+    },
+    "morerules": {
+        "ru": "С более подробным списком правил можно ознакомиться в дискорде",
+        "en": "A more detailed list of rules can be found in the discord",
+    },
+    "admins": {
+        "ru": "Администрация",
+        "en": "Admins",
+    },
+    "madeserver": {
+        "ru": "Создатели сервера",
+        "en": "The creators of the server",
+    },
+    "mader": {
+        "ru": "Основатель",
+        "en": "Founder",
+    },
+    "imper": {
+        "ru": "Имератор",
+        "en": "Imperator",
+    },
+    "primarch": {
+        "ru": "Примарх",
+        "en": "Primarch",
+    },
+    "priest": {
+        "ru": "Верховный жрец",
+        "en": "High priest",
+    },
+    "moder": {
+        "ru": "Созьдаль сайтикь",
+        "en": "Creator of the site",
     },
 }
